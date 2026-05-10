e0330d0301882cf220d992c8ecb31c1665608168
Kai Schmidt <kaikaliischmidt@gmail.com>
2026-05-09 18:26:52 -0700

    fix firefox persistence

--- home.nix
+++ home.nix
@@ -1,10 +1,25 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "kai";
-  home.homeDirectory = "/home/kai";
-  home.stateVersion = "25.05";
-  programs.bash.shellAliases = {
-    nrs = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
+  home = {
+    username = "kai";
+    homeDirectory = "/home/kai";
+    stateVersion = "25.05";
+    shellAliases = {
+      nrs = "sudo nixos-rebuild switch --flake ~/nixos-config#nixos";
+    };
+  };
+  programs = {
+    firefox = {
+      enable = true;
+      profiles.default = {
+        isDefault = true;
+        settings = {
+          "browser.startup.page" = 1;
+          "browser.sessionstore.resume_from_crash" = true;
+        };
+      };
+    };
+    bash.enable = true;
   };
 }
