91cb2ce0c62e16540f2f230d87fc1c427ec057b3
Kai Schmidt <kaikaliischmidt@gmail.com>
2026-05-21 23:59:14 -0700

    add gh

--- configuration.nix
+++ configuration.nix
@@ -86,6 +86,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     git
+    gh
     firefox
     home-manager
     curl
